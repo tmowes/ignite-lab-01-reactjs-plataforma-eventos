5637ada677503efce75de17b318611e35c43347d
tmowes <t-mowes@hotmail.com>
2022-06-20 18:56:07 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#FF6900',
+          400: '#F97316',
+          300: '#FB923C',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
+}
