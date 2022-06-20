832c3d156389cbb7844253cf7ae2338a34ef20e7
tmowes <t-mowes@hotmail.com>
2022-06-20 19:12:22 -0300

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export function App() {
   return (
     <div>
-      <h1>Julius</h1>
+      <div className="bg-zinc-900 p-4 rounded-2xl mb-4 flex flex-col items-center shadow-lg">
+        <h1>Julius</h1>
+      </div>
     </div>
   )
 }
